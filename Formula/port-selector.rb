2a152d64bd475a251587184ea00fc9eb6c722c3f
Danil Pismenny <dapi@users.noreply.github.com>
2026-01-26 22:43:52 +0300

    Update port-selector to v0.9.1

--- Formula/port-selector.rb
+++ Formula/port-selector.rb
@@ -1,33 +1,33 @@
 class PortSelector < Formula
   desc "Port allocator for parallel local dev environments"
   homepage "https://github.com/dapi/port-selector"
-  version "0.8.1"
+  version "0.9.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/dapi/port-selector/releases/download/v#{version}/port-selector_#{version}_darwin_arm64"
-      sha256 "600c5140e835ea85cba2b3823b83d6dc9a83e8475b126422e9cd2f1778ca1c1d"
+      url "https://github.com/dapi/port-selector/releases/download/v#{version}/port-selector-darwin-arm64"
+      sha256 "d7ca99546c873941d9f039243c425adcc2d1c81f65945bcaedb664503dfb4846"
     else
-      url "https://github.com/dapi/port-selector/releases/download/v#{version}/port-selector_#{version}_darwin_amd64"
-      sha256 "ad4a94c2cc5a3e0bc7fabeaffd1ec5894e42d21adfe8c032eec0c03c32e84747"
+      url "https://github.com/dapi/port-selector/releases/download/v#{version}/port-selector-darwin-amd64"
+      sha256 "5eace76a84293ee1e27648301622c578c12f0043db3e93aaad7cf7dc1a979988"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/dapi/port-selector/releases/download/v#{version}/port-selector_#{version}_linux_arm64"
-      sha256 "e80874f3d11edb101a51a35f2dc92d2faabdb49ac9d2dd8e68e9496118c1d680"
+      url "https://github.com/dapi/port-selector/releases/download/v#{version}/port-selector-linux-arm64"
+      sha256 "1c10f8fb43180bdb47aa1fd9280f98f8cad12193c384b9bc79af95dddbc0faab"
     else
-      url "https://github.com/dapi/port-selector/releases/download/v#{version}/port-selector_#{version}_linux_amd64"
-      sha256 "d68765028ccfeef88e91e0da7f902a4eaf3d33c26c54fa56191841c350ef81d1"
+      url "https://github.com/dapi/port-selector/releases/download/v#{version}/port-selector-linux-amd64"
+      sha256 "79b341af8e3dee5744b93d3d2172f530330a64d658d01bdfc092fec04940da95"
     end
   end
 
   def install
     binary_name = if OS.mac?
-      Hardware::CPU.arm? ? "port-selector_#{version}_darwin_arm64" : "port-selector_#{version}_darwin_amd64"
+      Hardware::CPU.arm? ? "port-selector-darwin-arm64" : "port-selector-darwin-amd64"
     else
-      Hardware::CPU.arm? ? "port-selector_#{version}_linux_arm64" : "port-selector_#{version}_linux_amd64"
+      Hardware::CPU.arm? ? "port-selector-linux-arm64" : "port-selector-linux-amd64"
     end
     bin.install binary_name => "port-selector"
   end
